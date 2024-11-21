57532a4cece974f5510f821636050c435f263bea
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-11-21 22:53:28 +0500

    added logic for reading and writing to shared pref

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_app/di.dart';
 import 'package:firebase_app/firebase_options.dart';
 import 'package:firebase_app/presentation/bloc/car_bloc.dart';
 import 'package:firebase_app/presentation/bloc/car_events.dart';
+import 'package:firebase_app/presentation/screens/car_list_screen.dart';
 import 'package:firebase_app/presentation/screens/onboarding_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logging/logging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
@@ -25,20 +27,24 @@ void main() async {
   } catch (e) {
     log.severe("Error in Dependency Injection: $e");
   }
-  runApp(const MyApp());
+  final prefs = await SharedPreferences.getInstance();
+  final bool? isOnboardingCompleted = prefs.getBool('onboardingCompleted');
+  runApp(MyApp(isOnboardingCompleted ?? false));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isOnboardingCompleted;
+  const MyApp(this.isOnboardingCompleted, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => sl<CarBloc>()..add(LoadCars()),
-      child: const MaterialApp(
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        home: OnboardingScreen(),
+        home:
+            isOnboardingCompleted ? CarListScreen() : const OnboardingScreen(),
       ),
     );
   }
--- lib/presentation/screens/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_app/presentation/screens/car_list_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -16,9 +17,7 @@ class OnboardingScreen extends StatelessWidget {
               decoration: const BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage('assets/images/onboarding.png'),
-                      fit: BoxFit.cover
-                  )
-              ),
+                      fit: BoxFit.cover)),
             ),
           ),
           Expanded(
@@ -28,32 +27,42 @@ class OnboardingScreen extends StatelessWidget {
               children: [
                 const Text(
                   'Premium cars. \nEnjoy the luxury',
-                  style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 const Text(
                   'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
                   style: TextStyle(color: Colors.grey, fontSize: 16),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 SizedBox(
                   width: 320,
                   height: 54,
                   child: ElevatedButton(
-                      onPressed: (){
+                      onPressed: () {
+                        SharedPreferences.getInstance().then((value) {
+                          value.setBool('onboardingCompleted', true);
+                        });
                         Navigator.of(context).pushAndRemoveUntil(
-                            MaterialPageRoute(builder: (context) => CarListScreen())
-                            , (route) => false);
+                            MaterialPageRoute(
+                                builder: (context) => CarListScreen()),
+                            (route) => false);
                       },
                       style: ElevatedButton.styleFrom(
                           foregroundColor: Colors.black,
-                          backgroundColor: Colors.white
-                      ),
+                          backgroundColor: Colors.white),
                       child: const Text(
                         'Let\'s Go',
-                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                      )
-                  ),
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      )),
                 )
               ],
             ),
